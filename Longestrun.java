15356f704572dc41c0f3dede9c5d74893ad0964c
Nick P-A <nicholas.pasco-arnone@mths.ca>
2020-04-19 22:57:06 +0000

    initial commit

--- Longestrun.java
+++ Longestrun.java
@@ -1,70 +1,55 @@
 import java.io.IOException;
 import java.io.StringReader;
 import java.util.Scanner;
+
 /**
  * This program calculates the longest run of a string.
  * 
  * @author Nick P-A
- * @since 2020/2/13 feb 25 2020
+ * @since 2020/4/19
  * @version 1.0
  */
  
 
 public class Longestrun {
-  static int maxcount = 0;
-  
+
+  static String userString;
+
   /**
-  * This class gets the string from the user and prints the answer.
+  * This gets the users input.
   */
-  
-  
+
+
   public static void main(String[] args) {
 
-    Scanner scanner = new Scanner(System.in);
-    System.out.println("Enter the string : ");
-    String string = scanner.next();
-    
-    if (string != null) {
-      String strout = longestrun(string);
-      System.out.println("longest run is : " + maxcount);
-    
-    } else {
-      System.out.println("Input string is empty");
-    }
-    scanner.close();
+    Scanner userInput = new Scanner(System.in);
+    System.out.println("enter a string to find the longest run: ");
+    userString = userInput.nextLine();
+    System.out.println(maxRun(userString));
   }
-  
-  
+
+
   /**
-  * This class calculates the longest run of a string.
+  * This calculates the longest run of a string.
   */
-  
-  public static String longestrun(String string) {
-    int pos = 0;
-    int maxpos = string.length();
-    char thiselem;
-    char lastelem;
-    String strout = "";
-    int count = 1;
-    
-    lastelem = string.charAt(pos);
-    for (pos = 1; pos < maxpos; pos++) {
-      thiselem = string.charAt(pos);
-      if (lastelem == thiselem) {
-        count++; 
-        
-      } else {
-        if (count > maxcount) {
-          maxcount = count;
-        }
-        count = 1;
+
+
+  public static int maxRun(String testString) {
+    int length = testString.length();
+    String substr = "";
+    for (int i = 0; i < length;) {
+      char current = testString.charAt(i);
+      String tempSubstr = "" + current;
+      while ((i + 1) < length && (current == testString.charAt(i + 1))) {
+        tempSubstr = tempSubstr + testString.charAt(++i);
       }
-      lastelem = thiselem;
+
+      if (tempSubstr.length() >= substr.length()) {
+        substr = tempSubstr;
+      }
+      i += 1;
     }
-    if (count > maxcount) {
-      maxcount = count;
-    }
-    String test = "";
-    return test;
+    System.out.println(substr);
+    return substr.length();
   }
 }
